86bbe3ed769e2d68af6aefa16e2ca694f93ffaeb
konstarina <55761341+konstarina@users.noreply.github.com>
2020-03-09 18:29:32 +0200

    Add files via upload

---
+++ nfa2dfa.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+rules = [ # Proper hardcode
+	'A-a-B',
+	'B-c-B',
+	'B-b-C',
+	'C-a-C',
+	'A-a-A',
+	'B-a-C'
+]
+
+def getNFA(rules):
+	nfa = {}
+	for rule in rules:
+		x = rule.split('-')
+
+		if not x[0] in nfa:
+			nfa[x[0]] = {}
+		
+		if not x[1] in nfa[x[0]]:
+			nfa[x[0]][x[1]] = ''
+		
+		nfa[x[0]][x[1]] += x[2]
+	return nfa
+
+def getStates(nfa):
+	states = []
+	for x in nfa:
+		states.append(x)
+
+	for x in nfa:
+		for y in nfa[x]:
+			if len(nfa[x][y]) > 1:
+				if not nfa[x][y] in states:
+					states.append(nfa[x][y])
+			else:
+				if not nfa[x][y][0] in states:
+					states.append(nfa[x][y][0])
+	return states
+
+def getMethods(nfa):
+	methods = []
+
+	for x in nfa:
+		for j in nfa[x]:
+			if not j in methods:
+				methods.append(j)
+
+	return methods
+
+def convertNFA2DFA(nfa, states, methods):
+	dfa = nfa.copy()
+	for s in states:
+		if not s in dfa:
+			separated = list(s)
+			for m in methods:
+				temp = []
+
+				for sp in separated:
+					if m in dfa[sp]:
+						temp.append(dfa[sp][m])
+
+				if not s in dfa:
+					dfa[s] = {}
+						
+				dfa[s][m] = ''.join(set(''.join(temp)))
+				states.append(''.join(set(''.join(temp))))
+
+	return dfa
+
+nfa = getNFA(rules)
+states = getStates(nfa)
+methods = getMethods(nfa)
+dfa = convertNFA2DFA(nfa, states, methods)
+
+df = pd.DataFrame(dfa)
+df = df.fillna("-")
+print(df.transpose())
---
+++ rfga.py
@@ -0,0 +1,94 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# fa = { # Hardcode. Like rock... but code.
+# 	'S':{ 
+# 		'a':'B'
+# 	},
+# 	'B':{ 
+# 		'b':'S',
+# 		'a':'C',
+# 		'c':'Z'
+# 	},
+# 	'C':{ 
+# 		'b':'D'
+# 	},
+# 	'D':{ 
+# 		'c':'Z',
+# 		'a':'C'
+# 	}
+# }
+
+rules = [ # Proper hardcode
+	'S-aB',
+	'B-bS',
+	'B-aC',
+	'B-c',
+	'C-bD',
+	'D-c',
+	'D-aC'
+]
+
+
+def drawGraph():
+	G = nx.MultiDiGraph()
+	G.add_edges_from(
+	[('S', 'B'), ('B', 'S'), ('B', 'C'), ('B', 'Z'), ('C', 'D'),
+	('D', 'Z'), ('D', 'C')])
+
+	# Need to create a layout when doing
+	# separate calls to draw nodes and edges
+	pos = nx.spring_layout(G)
+	nx.draw_networkx_nodes(G, pos)
+	nx.draw_networkx_labels(G, pos)
+	nx.draw_networkx_edges(G, pos, arrows=True, )
+	nx.draw_networkx_edge_labels(G, pos, edge_labels=
+		dict([
+			(('S', 'B'), 'a <---> b'),
+			(('B', 'C'), 'a'),
+			(('B', 'Z'), 'c'),
+			(('C', 'D'), 'b <---> a'),
+			(('D', 'Z'), 'c'),
+		])
+	)
+	plt.show()
+
+def getRules(rules):
+	fa = {}
+	for rule in rules:
+		uInput = rule.split('-')
+
+		x = {
+			0: list(uInput[0])[0],
+			1: list(uInput[1])
+		}
+
+		if not x[0] in fa:
+			fa[x[0]] = {}
+		if len(x[1]) == 1:
+			x[1].append('Z')
+		
+		fa[ x[0] ][ x[1][0] ] = x[1][1]
+	return fa
+
+def accepts(fa, initial, final, string):
+	state = initial
+	for x in string:
+		if state in fa and x in fa[state]:
+			state = fa[state][x]
+		else:
+			return False
+
+	return state in final
+
+drawGraph()
+
+fa = getRules(rules)
+print(fa)
+while True:
+	uInput = input('> ')
+
+	if uInput == '':
+		break
+
+	print(accepts(fa, 'S', {'Z'}, uInput)) 
